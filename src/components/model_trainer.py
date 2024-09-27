a7109a199d0983c3e8792dd911d8709fe08a88db
yamanmhj <yamanmhjr@gmail.com>
2024-09-27 20:11:13 +1000

    updates utils and model_trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,14 +14,14 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
-from src.exception import CustomException
+from src.exception import CustomeException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_config_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
@@ -46,7 +46,7 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            params={
+            Search_space={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                     # 'splitter':['best','random'],
@@ -69,6 +69,7 @@ class ModelTrainer:
                 "Linear Regression":{},
                 "XGBRegressor":{
                     'learning_rate':[.1,.01,.05,.001],
+                    "max_depth": [3,6,9],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "CatBoosting Regressor":{
@@ -85,35 +86,14 @@ class ModelTrainer:
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+                                             models=models,param=Search_space)
             
-            ## To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
+    
 
-            ## To get best model name from dict
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            best_model = models[best_model_name]
-
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
-
-            save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
-            )
-
-            predicted=best_model.predict(X_test)
-
-            r2_square = r2_score(y_test, predicted)
-            return r2_square
-            
+                      
 
 
 
             
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomeException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -29,26 +29,22 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            search_space = param[model_name]
 
-            grid_search = GridSearchCV(model,para,cv=3)
+            grid_search = GridSearchCV(model,search_space,cv=3)
             grid_search.fit(X_train,y_train)
 
-            model.set_params(**gs.best_params_)
+            model.set_params(**grid_search.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
-
+            report[model_name] = test_model_score
         return report
 
     except Exception as e:
